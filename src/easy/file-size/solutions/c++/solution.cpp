a061493146757895e651e7e96fb06c0fee2d9379
rdtsc <rdtsc@users.noreply.github.com>
2015-02-01 23:44:10 -0500

    Add a C++ solution to problem 26

---
+++ src/easy/file-size/solutions/c++/solution.cpp
@@ -0,0 +1,18 @@
+#include <cassert>
+#include <fstream>
+#include <iostream>
+
+int main(const int argc, const char* const argv[])
+{
+  // Getting away with no error checking throughout because CodeEval makes some
+  // strong guarantees about our runtime environment. No need to pay when we're
+  // being benchmarked. Don't forget to define NDEBUG prior to submitting!
+  assert(argc >= 2 && "Expecting at least one command-line argument.");
+
+  // Jump to the end of the (non-binary) stream.
+  std::ifstream inputStream(argv[1], std::ios::ate);
+
+  assert(inputStream && "Failed to open input stream.");
+
+  std::cout << inputStream.tellg() << '\n';
+}
