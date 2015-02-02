94c23749a8aa1f81cdf2c11fe194764af53c551b
rdtsc <rdtsc@users.noreply.github.com>
2015-02-02 13:27:07 -0500

    Add a C solution to problem 96

---
+++ src/easy/swap-case/solutions/c/solution.c
@@ -0,0 +1,33 @@
+#include <assert.h>
+#include <ctype.h>
+#include <stdio.h>
+
+int main(const int argc, const char* const argv[])
+{
+  // Getting away with no error checking throughout because CodeEval makes some
+  // strong guarantees about our runtime environment. No need to pay when we're
+  // being benchmarked. Don't forget to define NDEBUG prior to submitting!
+  assert(argc >= 2 && "Expecting at least one command-line argument.");
+
+  static char stdoutBuffer[4096] = "";
+
+  // Turn on full output buffering for stdout.
+  setvbuf(stdout, stdoutBuffer, _IOFBF, sizeof stdoutBuffer);
+
+  FILE* inputStream = fopen(argv[1], "r");
+
+  assert(inputStream && "Failed to open input stream.");
+
+  for(char lineBuffer[512] = "";
+      fgets(lineBuffer, sizeof lineBuffer, inputStream);)
+  {
+    for(char* c = lineBuffer; *c; ++c)
+    {
+      *c = isupper(*c) ? tolower(*c) : toupper(*c);
+    }
+
+    fputs(lineBuffer, stdout);
+  }
+
+  // The CRT takes care of cleanup.
+}
