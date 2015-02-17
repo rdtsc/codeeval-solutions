2a6c42d211759f49a1d72c1dc25670aabeca766c
rdtsc <rdtsc@users.noreply.github.com>
2015-02-17 12:24:57 -0500

    Add a C++ solution to problem 19

---
+++ src/easy/bit-positions/solutions/c++/solution.cpp
@@ -0,0 +1,45 @@
+#include <bitset>
+#include <cassert>
+#include <fstream>
+#include <iostream>
+#include <limits>
+#include <locale>
+#include <vector>
+
+int main(const int argc, const char* const argv[])
+{
+  // Getting away with no error checking throughout because CodeEval makes some
+  // strong guarantees about our runtime environment. No need to pay when we're
+  // being benchmarked. Don't forget to define NDEBUG prior to submitting!
+  assert(argc >= 2 && "Expecting at least one command-line argument.");
+
+  std::ifstream inputStream(argv[1]);
+
+  assert(inputStream && "Failed to open input stream.");
+
+  // Add commas to the list of stream delimiters.
+  inputStream.imbue(std::locale(inputStream.getloc(), []
+  {
+    using facet = std::ctype<char>;
+
+    const auto classicTableStart = facet::classic_table(),
+               classicTableEnd   = classicTableStart + facet::table_size;
+
+    static std::vector<facet::mask> categories(classicTableStart,
+                                               classicTableEnd);
+
+    categories[','] |= facet::space;
+
+    // Ref-counted upstream.
+    return new facet(categories.data());
+  }()));
+
+  std::boolalpha(std::cout);
+
+  for(unsigned n = 0, p = 0, q = 0; inputStream >> n >> p >> q;)
+  {
+    const std::bitset<std::numeric_limits<decltype(n)>::digits> bits(n);
+
+    std::cout << (bits[p - 1] == bits[q - 1]) << '\n';
+  }
+}
