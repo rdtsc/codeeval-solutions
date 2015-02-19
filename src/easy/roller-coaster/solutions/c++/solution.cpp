f740154d7eb67ac0fa2ed2e19332290e2b36650d
rdtsc <rdtsc@users.noreply.github.com>
2015-02-18 21:26:10 -0500

    Add a C++ solution to problem 156

---
+++ src/easy/roller-coaster/solutions/c++/solution.cpp
@@ -0,0 +1,33 @@
+#include <cassert>
+#include <cctype>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+int main(const int argc, const char* const argv[])
+{
+  // Getting away with no error checking throughout because CodeEval makes some
+  // strong guarantees about our runtime environment. No need to pay when we're
+  // being benchmarked. Don't forget to define NDEBUG prior to submitting!
+  assert(argc >= 2 && "Expecting at least one command-line argument.");
+
+  std::ifstream inputStream(argv[1]);
+
+  assert(inputStream && "Failed to open input stream.");
+
+  for(std::string line; std::getline(inputStream, line);)
+  {
+    // First letter should be uppercased.
+    bool shouldUppercase = true;
+
+    for(auto& c : line) if(::isalpha(c))
+    {
+      c = (shouldUppercase) ? (::toupper(c)) : (::tolower(c));
+
+      // Flip state for the next letter.
+      shouldUppercase = !shouldUppercase;
+    }
+
+    std::cout << line << '\n';
+  }
+}
