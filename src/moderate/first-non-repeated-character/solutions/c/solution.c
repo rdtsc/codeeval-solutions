2e29519981e5b2bfa5b89cba1bc154ab7a1cb858
rdtsc <rdtsc@users.noreply.github.com>
2015-02-19 13:12:13 -0500

    Add a C solution to problem 12

---
+++ src/moderate/first-non-repeated-character/solutions/c/solution.c
@@ -0,0 +1,50 @@
+#include <assert.h>
+#include <ctype.h>
+#include <stddef.h>
+#include <stdio.h>
+
+int main(const int argc, const char* const argv[])
+{
+  // Getting away with no error checking throughout because CodeEval makes some
+  // strong guarantees about our runtime environment. No need to pay when we're
+  // being benchmarked. Don't forget to define NDEBUG prior to submitting!
+  assert(argc >= 2 && "Expecting at least one command-line argument.");
+
+  static char stdoutBuffer[128] = "";
+
+  // Turn on full output buffering for stdout.
+  setvbuf(stdout, stdoutBuffer, _IOFBF, sizeof stdoutBuffer);
+
+  FILE* inputStream = fopen(argv[1], "r");
+
+  assert(inputStream && "Failed to open input stream.");
+
+  for(char lineBuffer[128] = "";
+      fgets(lineBuffer, sizeof lineBuffer, inputStream);)
+  {
+    // [A-Z] + [a-z]
+    char hitCount[52] = "";
+
+    // Count the number of occurances of each character.
+    for(const char* c = lineBuffer; *c && *c != '\n'; ++c)
+    {
+      if(islower(*c))      ++hitCount[*c - 'a'];
+      else if(isupper(*c)) ++hitCount[*c - 'A'];
+    }
+
+    // Dump the first character that's unique.
+    for(const char* c = lineBuffer; *c && *c != '\n'; ++c) if(isalpha(*c))
+    {
+      const size_t probe = *c - (islower(*c) ? 'a' : 'A');
+
+      if(hitCount[probe] == 1)
+      {
+        putchar(*c);
+        putchar('\n');
+        break;
+      }
+    }
+  }
+
+  // The CRT takes care of cleanup.
+}
