3a4286c19d3b3ba994c30db7a298a1882491edf7
rdtsc <rdtsc@users.noreply.github.com>
2015-02-28 22:47:29 -0500

    Add a C solution to problem 93

---
+++ src/easy/capitalize-words/solutions/c/solution.c
@@ -0,0 +1,37 @@
+#include <assert.h>
+#include <ctype.h>
+#include <stdio.h>
+
+int main(const int argc, const char* const argv[])
+{
+  // Getting away with no error checking throughout because CodeEval makes some
+  // strong guarantees about our runtime environment. No need to pay when we're
+  // being benchmarked. Don't forget to define NDEBUG prior to submitting!
+  assert(argc >= 2 && "Expecting at least one command-line argument.");
+
+  static char stdoutBuffer[1024] = "";
+
+  // Turn on full output buffering for stdout.
+  setvbuf(stdout, stdoutBuffer, _IOFBF, sizeof stdoutBuffer);
+
+  FILE* inputStream = fopen(argv[1], "r");
+
+  assert(inputStream && "Failed to open input stream.");
+
+  for(char lineBuffer[256] = "";
+      fgets(lineBuffer, sizeof lineBuffer, inputStream);)
+  {
+    // Capitalize the first letter.
+    *lineBuffer = toupper(*lineBuffer);
+
+    for(char* c = lineBuffer + 1; *c; ++c)
+    {
+      // Capitalize the current letter if a space precedes it.
+      if(*(c - 1) == ' ') *c = toupper(*c);
+    }
+
+    fputs(lineBuffer, stdout);
+  }
+
+  // The CRT takes care of cleanup.
+}
