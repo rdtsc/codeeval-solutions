05f3fa85f01e7001d222adea3626906d24790b34
rdtsc <rdtsc@users.noreply.github.com>
2015-03-23 21:30:50 -0400

    Add a C solution to problem 62

---
+++ src/easy/n-mod-m/solutions/c/solution.c
@@ -0,0 +1,26 @@
+#include <assert.h>
+#include <stdio.h>
+
+int main(const int argc, const char* const argv[])
+{
+  // Getting away with no error checking throughout because CodeEval makes some
+  // strong guarantees about our runtime environment. No need to pay when we're
+  // being benchmarked. Don't forget to define NDEBUG prior to submitting!
+  assert(argc >= 2 && "Expecting at least one command-line argument.");
+
+  static char stdoutBuffer[128] = "";
+
+  // Turn on full output buffering for stdout.
+  setvbuf(stdout, stdoutBuffer, _IOFBF, sizeof stdoutBuffer);
+
+  FILE* inputStream = fopen(argv[1], "r");
+
+  assert(inputStream && "Failed to open input stream.");
+
+  for(unsigned n = 0, m = 0; fscanf(inputStream, "%u,%u", &n, &m) == 2;)
+  {
+    printf("%u\n", n - (n / m) * m);
+  }
+
+  // The CRT takes care of cleanup.
+}
