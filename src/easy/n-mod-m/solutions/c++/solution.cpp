352c7b165e34b7029f2e21987049daf38909e86a
rdtsc <rdtsc@users.noreply.github.com>
2015-03-23 21:30:54 -0400

    Add a C++ solution to problem 62

---
+++ src/easy/n-mod-m/solutions/c++/solution.cpp
@@ -0,0 +1,23 @@
+#include <cassert>
+#include <fstream>
+#include <iostream>
+
+int main(const int argc, const char* const argv[])
+{
+  // Getting away with no error checking throughout because CodeEval makes some
+  // strong guarantees about our runtime environment. No need to pay when we're
+  // being benchmarked. Don't forget to define NDEBUG prior to submitting!
+  assert(argc >= 2 && "Expecting at least one command-line argument.");
+
+  std::ifstream inputStream(argv[1]);
+
+  assert(inputStream && "Failed to open input stream.");
+
+  for(unsigned n = 0, m = 0; inputStream >> n;)
+  {
+    inputStream.ignore();
+    inputStream >> m;
+
+    std::cout << (n - (n / m) * m) << '\n';
+  }
+}
