f8c8b4f8434995ccf23e6977b75b313164577fda
rdtsc <rdtsc@users.noreply.github.com>
2015-05-18 23:41:12 -0400

    Add a C++ solution to problem 33

---
+++ src/moderate/double-squares/solutions/c++/solution.cpp
@@ -0,0 +1,44 @@
+#include <cassert>
+#include <cmath>
+#include <fstream>
+#include <iostream>
+
+int main(const int argc, const char* const argv[])
+{
+  // Getting away with no error checking throughout because CodeEval makes some
+  // strong guarantees about our runtime environment. No need to pay when we're
+  // being benchmarked. Don't forget to define NDEBUG prior to submitting!
+  assert(argc >= 2 && "Expecting at least one command-line argument.");
+
+  std::ifstream inputStream(argv[1]);
+
+  assert(inputStream && "Failed to open input stream.");
+
+  using value_type = unsigned;
+
+  value_type n = 0;
+
+  for(inputStream >> n; inputStream >> n;)
+  {
+    const value_type upperLimit = ::sqrt(n);
+
+    auto result = [&]
+    {
+      unsigned result = 0;
+
+      for(auto i = decltype(upperLimit){}; i <= upperLimit; ++i)
+      {
+        const value_type value = (n - i * i),
+                         test  = ::sqrt(value);
+
+        if(test < i) return result;
+
+        else if(test * test == value) ++result;
+      }
+
+      return result;
+    }();
+
+    std::cout << result << '\n';
+  }
+}
