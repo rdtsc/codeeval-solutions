68e86bc8640ee28f246af9e5e649864210887fc3
rdtsc <rdtsc@users.noreply.github.com>
2015-09-25 11:51:12 -0400

    Add a C++ solution to problem 215

---
+++ src/moderate/double-trouble/solutions/c++/solution.cpp
@@ -0,0 +1,54 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <string>
+
+static std::uint_least64_t getVariantCount(const std::string& data)
+{
+  assert(!(data.size() & 1));
+
+  assert(std::all_of(data.cbegin(), data.cend(), [](const char c)
+  {
+    if(c == 'A' || c == 'B' || c == '*') return true;
+
+    return false;
+  }));
+
+  const auto dataLeft  = data.cbegin(),
+             dataRight = std::next(dataLeft, (data.size() / 2));
+
+  unsigned asteriskCount = 0;
+
+  for(auto lhs = dataLeft, rhs = dataRight; lhs != dataRight; ++lhs, ++rhs)
+  {
+    // Wildcard pair.
+    if((*lhs == '*') && (*rhs == '*')) ++asteriskCount;
+
+    // Complete mismatch between letters.
+    else if((*lhs != *rhs) && !(*lhs == '*' || *rhs == '*')) return 0;
+  }
+
+  // |{A, B}| = 2.
+  return ::pow(2, asteriskCount);
+}
+
+int main(const int argc, const char* const argv[])
+{
+  // Getting away with no error checking throughout because CodeEval makes some
+  // strong guarantees about our runtime environment. No need to pay when we're
+  // being benchmarked. Don't forget to define NDEBUG prior to submitting!
+  assert(argc >= 2 && "Expecting at least one command-line argument.");
+
+  std::ifstream inputStream(argv[1]);
+
+  assert(inputStream && "Failed to open input stream.");
+
+  for(std::string line; std::getline(inputStream, line);)
+  {
+    std::cout << ::getVariantCount(line) << '\n';
+  }
+}
