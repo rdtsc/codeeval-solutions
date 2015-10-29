7c1ad20b3c59cc387e9345e619d8373a79f491ab
rdtsc <rdtsc@users.noreply.github.com>
2015-10-29 00:15:15 -0400

    Refactor

--- src/easy/age-distribution/solutions/python/solution.py
+++ src/easy/age-distribution/solutions/python/solution.py
@@ -2,28 +2,14 @@
 
 import sys
 
-category = \
-[
-  "This program is for humans",
-  "Still in Mama's arms",
-  "Preschool Maniac",
-  "Elementary school",
-  "Middle school",
-  "High school",
-  "College",
-  "Working for the man",
-  "The Golden Years"
-]
+classify = lambda age: "Still in Mama's arms" if age <=  2 else \
+                       "Preschool Maniac"     if age <=  4 else \
+                       "Elementary school"    if age <= 11 else \
+                       "Middle school"        if age <= 14 else \
+                       "High school"          if age <= 18 else \
+                       "College"              if age <= 22 else \
+                       "Working for the man"  if age <= 65 else \
+                       "The Golden Years"
 
-classify = lambda age: category[0] if age <   0 else \
-                       category[0] if age > 100 else \
-                       category[1] if age <=  2 else \
-                       category[2] if age <=  4 else \
-                       category[3] if age <= 11 else \
-                       category[4] if age <= 14 else \
-                       category[5] if age <= 18 else \
-                       category[6] if age <= 22 else \
-                       category[7] if age <= 65 else \
-                       category[8]
-
-print(*(map(classify, (int(n) for n in sys.stdin))), sep='\n')
+for age in (int(age) for age in sys.stdin):
+  print('This program is for humans' if 0 < age > 100 else classify(age))
