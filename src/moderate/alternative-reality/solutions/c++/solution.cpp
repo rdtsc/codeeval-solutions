5b6949bd2e79ad4d13a6fdc3933e20dab27e29b2
rdtsc <rdtsc@users.noreply.github.com>
2015-11-20 12:13:30 -0500

    Refactor LUT

--- src/moderate/alternative-reality/solutions/c++/solution.cpp
+++ src/moderate/alternative-reality/solutions/c++/solution.cpp
@@ -23,6 +23,13 @@ static constexpr unsigned getCombinationCount(const int value,
   #undef SELF
 }
 
+static constexpr unsigned denominations[] = {1, 5, 10, 25, 50};
+
+static constexpr unsigned operator"" _(const unsigned long long n)
+{
+  return ::getCombinationCount(n, denominations);
+}
+
 int main(const int argc, const char* const argv[])
 {
   // Getting away with no error checking throughout because CodeEval makes some
@@ -34,8 +41,6 @@ int main(const int argc, const char* const argv[])
 
   assert(inputStream && "Failed to open input stream.");
 
-  static constexpr unsigned denominations[] = {1, 5, 10, 25, 50};
-
   // This problem's upper bound is quite small. Offloading the number crunching
   // to the compiler seems appropriate.
   static constexpr std::array<unsigned, 100> lut =
@@ -43,36 +48,20 @@ int main(const int argc, const char* const argv[])
     /*
       #!/usr/bin/env python3
 
-      from decimal import Decimal
-
       for i in range(1, 101):
-        print('V(0x{:02X})'.format(i), end=', ' if i % 5 else ',\n')
+        print('{:>2}_'.format(i), end=', ' if i % 10 else ',\n')
     */
 
-    #define V(Value) ::getCombinationCount((Value), denominations)
-
-    V(0x01), V(0x02), V(0x03), V(0x04), V(0x05),
-    V(0x06), V(0x07), V(0x08), V(0x09), V(0x0A),
-    V(0x0B), V(0x0C), V(0x0D), V(0x0E), V(0x0F),
-    V(0x10), V(0x11), V(0x12), V(0x13), V(0x14),
-    V(0x15), V(0x16), V(0x17), V(0x18), V(0x19),
-    V(0x1A), V(0x1B), V(0x1C), V(0x1D), V(0x1E),
-    V(0x1F), V(0x20), V(0x21), V(0x22), V(0x23),
-    V(0x24), V(0x25), V(0x26), V(0x27), V(0x28),
-    V(0x29), V(0x2A), V(0x2B), V(0x2C), V(0x2D),
-    V(0x2E), V(0x2F), V(0x30), V(0x31), V(0x32),
-    V(0x33), V(0x34), V(0x35), V(0x36), V(0x37),
-    V(0x38), V(0x39), V(0x3A), V(0x3B), V(0x3C),
-    V(0x3D), V(0x3E), V(0x3F), V(0x40), V(0x41),
-    V(0x42), V(0x43), V(0x44), V(0x45), V(0x46),
-    V(0x47), V(0x48), V(0x49), V(0x4A), V(0x4B),
-    V(0x4C), V(0x4D), V(0x4E), V(0x4F), V(0x50),
-    V(0x51), V(0x52), V(0x53), V(0x54), V(0x55),
-    V(0x56), V(0x57), V(0x58), V(0x59), V(0x5A),
-    V(0x5B), V(0x5C), V(0x5D), V(0x5E), V(0x5F),
-    V(0x60), V(0x61), V(0x62), V(0x63), V(0x64)
-
-    #undef V
+     1_,  2_,  3_,  4_,  5_,  6_,  7_,  8_,  9_, 10_,
+    11_, 12_, 13_, 14_, 15_, 16_, 17_, 18_, 19_, 20_,
+    21_, 22_, 23_, 24_, 25_, 26_, 27_, 28_, 29_, 30_,
+    31_, 32_, 33_, 34_, 35_, 36_, 37_, 38_, 39_, 40_,
+    41_, 42_, 43_, 44_, 45_, 46_, 47_, 48_, 49_, 50_,
+    51_, 52_, 53_, 54_, 55_, 56_, 57_, 58_, 59_, 60_,
+    61_, 62_, 63_, 64_, 65_, 66_, 67_, 68_, 69_, 70_,
+    71_, 72_, 73_, 74_, 75_, 76_, 77_, 78_, 79_, 80_,
+    81_, 82_, 83_, 84_, 85_, 86_, 87_, 88_, 89_, 90_,
+    91_, 92_, 93_, 94_, 95_, 96_, 97_, 98_, 99_, 100_
   };
 
   for(unsigned n = 0; inputStream >> n;)
