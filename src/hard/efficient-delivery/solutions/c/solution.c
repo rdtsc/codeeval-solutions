5957d5b84f5c74399026e70b5415fdad9007f973
rdtsc <rdtsc@users.noreply.github.com>
2015-12-07 13:38:41 -0500

    Add additional assertion

--- src/hard/efficient-delivery/solutions/c/solution.c
+++ src/hard/efficient-delivery/solutions/c/solution.c
@@ -170,6 +170,8 @@ int main(const int argc, const char* const argv[])
       values[valueCount++] = extractUint(&cursor);
     }
 
+    assert(valueCount);
+
     const unsigned targetVolume = values[--valueCount];
 
     dumpDeliveryOptions(values, valueCount, targetVolume, stdout);
