c7d9ac9b98ed7aa0ed1b23661f17fd15c9841ed9
rdtsc <rdtsc@users.noreply.github.com>
2015-12-18 08:18:28 -0500

    Refactor

--- src/easy/real-fake/solutions/python/solution.py
+++ src/easy/real-fake/solutions/python/solution.py
@@ -5,6 +5,6 @@ import sys
 for line in (line.rstrip() for line in sys.stdin):
   line = [int(n) for n in line.replace(' ', '')]
 
-  tally = sum(n * 2 for n in line[0::2]) + sum(n for n in line[1::2])
+  tally = sum(n * 2 for n in line[::2]) + sum(line[1::2])
 
   print('Fake' if tally % 10 else 'Real')
