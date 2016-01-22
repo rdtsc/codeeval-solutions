de8790bdb35150d2f6b8557453a0c33ac6ad090c
rdtsc <rdtsc@users.noreply.github.com>
2016-01-22 06:55:05 -0500

    Add a Python solution to problem 232

---
+++ src/easy/not-so-clever/solutions/python/solution.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import sys
+
+with open(sys.argv[1]) as file:
+  for line in (line.rstrip().replace('|', ' ') for line in file):
+    values = list(map(int, line.split()))
+
+    for i in range(values.pop()):
+      for j in range(len(values) - 1):
+        if values[j] > values[j + 1]:
+          values[j], values[j + 1] = values[j + 1], values[j]
+          break
+
+    print(*values, sep=' ')
