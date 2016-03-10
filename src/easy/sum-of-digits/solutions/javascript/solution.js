47de9b27a8e8919ed4f0ca33148aeda665bccbb7
rdtsc <rdtsc@users.noreply.github.com>
2016-03-09 21:44:43 -0500

    Add a JavaScript solution to problem 21

---
+++ src/easy/sum-of-digits/solutions/javascript/solution.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const lineReader = require('readline').createInterface(
+{
+  input: require('fs').createReadStream(process.argv[2])
+});
+
+lineReader.on('line', (line) =>
+{
+  line = line.split('').map(Number);
+
+  console.log(line.reduce((a, b) => a + b));
+});
