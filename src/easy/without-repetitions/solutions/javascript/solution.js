cd1d73cab7cc1928ccb4269adb0dedb7b199ecfa
rdtsc <rdtsc@users.noreply.github.com>
2016-03-09 21:46:48 -0500

    Add a JavaScript solution to problem 173

---
+++ src/easy/without-repetitions/solutions/javascript/solution.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const lineReader = require('readline').createInterface(
+{
+  input: require('fs').createReadStream(process.argv[2])
+});
+
+lineReader.on('line', (line) =>
+{
+  console.log(line.replace(/(.)\1+/g, '$1'));
+});
