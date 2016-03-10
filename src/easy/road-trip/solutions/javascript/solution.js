b79c579ba75dd00ba1733f23140b5e4222ba2623
rdtsc <rdtsc@users.noreply.github.com>
2016-03-10 10:14:37 -0500

    Add a JavaScript solution to problem 124

---
+++ src/easy/road-trip/solutions/javascript/solution.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const lineReader = require('readline').createInterface(
+{
+  input: require('fs').createReadStream(process.argv[2])
+});
+
+lineReader.on('line', (line) =>
+{
+  line = line.replace(/[^\d\s]/g, '')
+             .split(' ')
+             .map(Number)
+             .sort((a, b) => a - b);
+
+  let distances = [line[0]];
+
+  for(let i = 1; i < line.length; ++i)
+  {
+
+    distances.push(line[i] - line[i - 1]);
+  }
+
+  console.log(distances.join(','));
+});
