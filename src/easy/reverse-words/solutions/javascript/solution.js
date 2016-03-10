dae09da12bf785c9d0d93ffa8c9976de615a9888
rdtsc <rdtsc@users.noreply.github.com>
2016-03-09 21:43:04 -0500

    Add a JavaScript solution to problem 8

---
+++ src/easy/reverse-words/solutions/javascript/solution.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const lineReader = require('readline').createInterface(
+{
+  input: require('fs').createReadStream(process.argv[2])
+});
+
+lineReader.on('line', (line) =>
+{
+  console.log(line.split(' ').reverse().join(' '));
+});
