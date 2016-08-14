47ef1d500e41195d69aef339dbce1dbc70844e54
rdtsc <rdtsc@users.noreply.github.com>
2016-08-14 08:18:16 -0400

    Add a PHP solution to problem 21

---
+++ src/easy/sum-of-digits/solutions/php/solution.php
@@ -0,0 +1,13 @@
+#!/usr/bin/env php
+<?php
+
+$inputFile = new SplFileObject($argv[1]);
+
+$inputFile->setFlags(SplFileObject::READ_AHEAD |
+                     SplFileObject::SKIP_EMPTY |
+                     SplFileObject::DROP_NEW_LINE);
+
+foreach($inputFile as $n)
+{
+  echo array_sum(str_split($n)), PHP_EOL;
+}
