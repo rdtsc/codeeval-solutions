aa4d416b719d64b1fc7dacc86dc82a3f1a70050d
rdtsc <rdtsc@users.noreply.github.com>
2016-08-14 08:56:45 -0400

    Add a PHP solution to problem 29

---
+++ src/easy/unique-elements/solutions/php/solution.php
@@ -0,0 +1,18 @@
+#!/usr/bin/env php
+<?php
+
+$inputFile = new SplFileObject($argv[1]);
+
+$inputFile->setFlags(SplFileObject::READ_CSV   |
+                     SplFileObject::READ_AHEAD |
+                     SplFileObject::SKIP_EMPTY |
+                     SplFileObject::DROP_NEW_LINE);
+
+foreach($inputFile as $values)
+{
+  sort($values);
+
+  $values = array_unique($values);
+
+  echo join(',', $values), PHP_EOL;
+}
