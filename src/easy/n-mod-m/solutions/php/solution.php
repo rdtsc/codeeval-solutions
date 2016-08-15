41a3150bde9cfaaac5defa0edcb91d10ac0862ee
rdtsc <rdtsc@users.noreply.github.com>
2016-08-15 08:43:04 -0400

    Add a PHP solution to problem 62

---
+++ src/easy/n-mod-m/solutions/php/solution.php
@@ -0,0 +1,16 @@
+#!/usr/bin/env php
+<?php
+
+$inputFile = new SplFileObject($argv[1]);
+
+$inputFile->setFlags(SplFileObject::READ_CSV   |
+                     SplFileObject::READ_AHEAD |
+                     SplFileObject::SKIP_EMPTY |
+                     SplFileObject::DROP_NEW_LINE);
+
+foreach($inputFile as $line)
+{
+  list($n, $m) = $line;
+
+  echo ($n - floor($n / $m) * $m), PHP_EOL;
+}
