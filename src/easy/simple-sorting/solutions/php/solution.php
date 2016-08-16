014420b6ef09bef2f4509ed913f5c95c85630ec2
rdtsc <rdtsc@users.noreply.github.com>
2016-08-16 11:39:47 -0400

    Add a PHP solution to problem 91

---
+++ src/easy/simple-sorting/solutions/php/solution.php
@@ -0,0 +1,18 @@
+#!/usr/bin/env php
+<?php
+
+$inputFile = new SplFileObject($argv[1]);
+
+$inputFile->setFlags(SplFileObject::READ_CSV   |
+                     SplFileObject::READ_AHEAD |
+                     SplFileObject::SKIP_EMPTY |
+                     SplFileObject::DROP_NEW_LINE);
+
+$inputFile->setCsvControl(' ');
+
+foreach($inputFile as $terms)
+{
+  sort($terms);
+
+  echo join(' ', $terms), PHP_EOL;
+}
