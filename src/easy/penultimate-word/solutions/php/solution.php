8c53e03162a29bd3eaa1139308e4745740345864
rdtsc <rdtsc@users.noreply.github.com>
2016-08-16 11:43:11 -0400

    Add a PHP solution to problem 92

---
+++ src/easy/penultimate-word/solutions/php/solution.php
@@ -0,0 +1,16 @@
+#!/usr/bin/env php
+<?php
+
+$inputFile = new SplFileObject($argv[1]);
+
+$inputFile->setFlags(SplFileObject::READ_CSV   |
+                     SplFileObject::READ_AHEAD |
+                     SplFileObject::SKIP_EMPTY |
+                     SplFileObject::DROP_NEW_LINE);
+
+$inputFile->setCsvControl(' ');
+
+foreach($inputFile as $line)
+{
+  echo $line[count($line) - 2], PHP_EOL;
+}
